6fa903295f26c5324b48cf70c8dde3bc201ac5d7
Geekytaurus <67371648+geekytaurus115@users.noreply.github.com>
2024-10-18 16:55:22 +0530

    Initial commit

---
+++ BasicProjects/HelloAndroid/app/src/main/java/com/geekytaurus/helloandroid/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.geekytaurus.helloandroid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.geekytaurus.helloandroid.ui.theme.HelloAndroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HelloAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        color = Color.White,
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+            .background(
+                Color.Blue,
+                shape = RoundedCornerShape(50.dp)
+            )
+            .padding(40.dp)
+    )
+}
